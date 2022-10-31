5efa56401a3d8a6a65d3b21b4f6ef6591ee776c8
bouyosri <yosri.bouzid@esprit.tn>
2022-10-31 02:00:32 +0100

    first commit

---
+++ Domain/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain
+{
+    public class Address
+    {
+        [Key]
+        public int Id { get; set; }
+        //public NomComplet NomComplet { get; set; }
+        public string Street { get; set; }
+        public int House_nb { get; set; }
+        public string Zip_code { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+    }
+}
